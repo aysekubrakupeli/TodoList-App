c44476944aaabb1eb9635fdce7501caeff499bdb
Ayse Kubra <aysekubrakupeli@gmail.com>
2017-11-10 16:41:03 +0000

    Added priority field

--- todoitem/models.py
+++ todoitem/models.py
@@ -5,4 +5,8 @@ from django.db import models
 class TodoItem(models.Model):
     name = models.CharField(max_length=30, blank=False)
     done = models.BooleanField(blank=False, default=False)
+    priority = models.CharField(max_length=6, blank=False)
     
+    def __str__(self):
+        return self.name
+
